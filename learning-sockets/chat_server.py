98fec256d3236933606de41aa3f94db89a59b549
pingrunhuang <pingrunhuang@gmail.com>
2020-01-30 14:59:44 +0800

    update chat client

--- learning-sockets/chat_server.py
+++ learning-sockets/chat_server.py
@@ -19,7 +19,7 @@ import socket
 import select
 
 IP = '127.0.0.1'
-PORT = '1234'
+PORT = 1234
 HEADERSIZE = 16
 
 def receive_msg(client_socket):
@@ -36,8 +36,13 @@ def receive_msg(client_socket):
         return None
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((socket.gethostname(), 1234))
-server_socket.listen(5)
+
+# SO_ - socket option
+# SOL_ - socket option level
+# Sets REUSEADDR (as a socket option) to 1 on socket
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((IP, PORT))
+server_socket.listen()
 
 sockets_list = [server_socket]
 # {client_socket_obj: data_in_communication}
@@ -53,11 +58,13 @@ while True:
     #   - writing - sockets ready for data to be send thru them
     #   - errors  - sockets with some exceptions
     # This is a blocking call, code execution will "wait" here and "get" notified in case any action should be taken
+    print("Server started")
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
     for notified_socket in read_sockets:
         if notified_socket == server_socket:
             # means a new client connected to the chat server
             client_socket, client_addr = notified_socket.accept()
+            print("{} joint chatting...".format(client_addr))
             user = receive_msg(client_socket)
             if not user:
                 # some error happen in the client side 
@@ -80,6 +87,7 @@ while True:
             # broadcast to all the other client except the sender
             for client_socket in clients:
                 if client_socket != notified_socket:
+                    # this can ensure that we can receive 2 times at client side with each length of HEADER_LENGTH
                     client_socket.send(user['header'] + user['data'] + msg['header'] + msg['data'])
             
     for notified_socket in exception_sockets:
